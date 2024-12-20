738d846ea0ece43b2736d1a58e62b7b24e6f59ff
jimdel <jdelay.jr@gmail.com>
2024-12-20 15:35:58 -0500

    feat: add hot reloading

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "jimdel/pkg/server"
+import (
+	"fmt"
+	"jimdel/pkg/server"
+	"log"
+	"os"
 
-const PORT = ":3000"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
-	err := server.Run(PORT)
+	port := ":" + os.Getenv("APP_PORT")
+	fmt.Println("Server running on port", port)
+	err := server.Run(port)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.22.2
 require (
 	github.com/a-h/templ v0.2.793
 	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
 )
